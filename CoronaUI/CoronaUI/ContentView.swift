6f04103ed8deb6856be7338d6253b0fea673e8c3
luizguerra <luizpedrofg@gmail.com>
2020-04-07 14:57:21 -0300

    new xcode added

---
+++ CoronaUI/CoronaUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaUI
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 07/04/20.
+//  Copyright © 2020 LuizGuerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
