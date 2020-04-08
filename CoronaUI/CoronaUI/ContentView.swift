d3cf1783c3ad7349cf72b0f80d474914c8de7bfd
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-04-08 17:28:00 -0300

    tab bar

--- CoronaUI/CoronaUI/ContentView.swift
+++ CoronaUI/CoronaUI/ContentView.swift
@@ -8,14 +8,53 @@
 
 import SwiftUI
 
+enum ViewSelected: String, CaseIterable {
+    
+    static var allCases: [ViewSelected] {
+        return [ .dashboard, .pacientes, .agenda, .informacoes]
+    }
+    
+    case dashboard
+    case pacientes
+    case agenda
+    case informacoes
+}
+
 struct ContentView: View {
+    
+    @State var showingDashboard = false
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        VStack {
+          ForEach (ViewSelected.allCases, id: \.self) { view in
+                
+                VStack {
+                    botao(viewName: view.rawValue)
+                }
+            }
+        }.padding()
+        .frame(width: 80, height: 600)
+        .background(Color(red: 50/255, green: 50/255, blue: 50/255))
+            
+            
+        
+    }
+}
+
+struct botao: View {
+    @State var viewName: String = ""
+    var body: some View {
+        VStack {
+            Image(viewName).resizable().frame(width: 64, height: 64)
+            Text(viewName)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 1366, height: 1024))
+        
     }
 }
