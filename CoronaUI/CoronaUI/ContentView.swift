08f24ac36029a817432c899d0bca84ea01229b10
Ronald Maciel <ronaldsmaciel2@outlook.com>
2020-04-09 22:39:42 -0300

    finished the tab bar layout

--- CoronaUI/CoronaUI/ContentView.swift
+++ CoronaUI/CoronaUI/ContentView.swift
@@ -26,29 +26,65 @@ struct ContentView: View {
     var body: some View {
         
         
-        VStack {
-          ForEach (ViewSelected.allCases, id: \.self) { view in
-                
-                VStack {
-                    botao(viewName: view.rawValue)
+        HStack {
+            VStack {
+                // Image Logo View
+                VStack{
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 56, height: 56)
+                        .padding(.bottom, 100)
                 }
-            }
-        }.padding()
-        .frame(width: 80, height: 600)
-        .background(Color(red: 50/255, green: 50/255, blue: 50/255))
+                
+                // Tab Bar View
+                ForEach (ViewSelected.allCases, id: \.self) { view in
+                    
+                    VStack {
+                        botao(viewName: view.rawValue)
+                        
+                        Spacer()
+                    }
+                }
+                
+                Spacer()
+                
+                // Logo RS View
+                VStack {
+                    
+                    Spacer(minLength: 266) // set the distance
+                    Image("logo_RS")
+                        .resizable()
+                        .frame(width: 64, height: 46)
+                }
+                
+            }.padding()
+                .frame(width: 116, height: 1024)
+                .background(Color(red: 50/255, green: 50/255, blue: 50/255))
             
             
-        
+            Text("hi")
+        }
     }
 }
 
 struct botao: View {
     @State var viewName: String = ""
+    
     var body: some View {
+        
         VStack {
-            Image(viewName).resizable().frame(width: 64, height: 64)
+            Image(viewName).resizable()
+                .frame(width: 64, height: 64)
+            
             Text(viewName)
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .frame(width: 116, height: 24)
+            
         }
+        
+        
+        
     }
 }
 
