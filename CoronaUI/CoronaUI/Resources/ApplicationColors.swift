4fdb471eb3515ff8861948ab8c06aae9c34df65d
luizguerra <luizpedrofg@gmail.com>
2020-04-13 17:44:35 -0300

    Global application colors

---
+++ CoronaUI/CoronaUI/Resources/ApplicationColors.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationColors.swift
+//  CoronaUI
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 13/04/20.
+//  Copyright © 2020 LuizGuerra. All rights reserved.
+//
+
+import Foundation
---
+++ CoronaUI/CoronaUI/Views/Pacientes/Subviews/PacientCell.swift
@@ -0,0 +1,21 @@
+//
+//  PacientCell.swift
+//  CoronaUI
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 13/04/20.
+//  Copyright © 2020 LuizGuerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct PacientCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PacientCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PacientCell()
+    }
+}
