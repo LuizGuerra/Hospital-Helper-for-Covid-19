357e62f60aed301cb9b6a0abc3bbc58c9ff83ac7
Ronald Maciel <ronaldsmaciel2@outlook.com>
2020-04-14 14:25:01 -0300

    Organize the files for PatientDetails

---
+++ CoronaUI/CoronaUI/Views/Pacientes/PacientesDetails/SubviewsPacientesDetails/Dados&Sintomas.swift
@@ -0,0 +1,21 @@
+//
+//  Dados&Sintomas.swift
+//  CoronaUI
+//
+//  Created by Ronald Maciel on 14/04/20.
+//  Copyright © 2020 LuizGuerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct Dados_Sintomas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Dados_Sintomas_Previews: PreviewProvider {
+    static var previews: some View {
+        Dados_Sintomas()
+    }
+}
