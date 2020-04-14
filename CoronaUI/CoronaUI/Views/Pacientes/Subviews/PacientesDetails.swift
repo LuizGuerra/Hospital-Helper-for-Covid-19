179564acdac3e6b44da7c103df38b541573e7e86
Ronald Maciel <ronaldsmaciel2@outlook.com>
2020-04-14 14:20:18 -0300

    Started the PatientsDetail screen

---
+++ CoronaUI/CoronaUI/Views/Pacientes/Subviews/PacientesDetails.swift
@@ -0,0 +1,21 @@
+//
+//  PacientesDetails.swift
+//  CoronaUI
+//
+//  Created by Ronald Maciel on 14/04/20.
+//  Copyright © 2020 LuizGuerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct PacientesDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PacientesDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PacientesDetails()
+    }
+}
