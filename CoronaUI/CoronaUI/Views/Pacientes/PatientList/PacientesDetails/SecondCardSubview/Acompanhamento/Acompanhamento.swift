4fbc7c53168b4e981f8bf02ce37554f7d5e4e3da
Ronald Maciel <ronaldsmaciel2@outlook.com>
2020-04-16 15:47:37 -0300

    Acompanhemento Screen HeadCard layout

--- CoronaUI/CoronaUI/Views/Pacientes/PatientList/PacientesDetails/SecondCardSubview/Acompanhamento/Acompanhamento.swift
+++ CoronaUI/CoronaUI/Views/Pacientes/PatientList/PacientesDetails/SecondCardSubview/Acompanhamento/Acompanhamento.swift
@@ -11,7 +11,54 @@ import SwiftUI
 struct Acompanhamento: View {
     var body: some View {
         HStack {
-            Color.red
+            ScrollView(.horizontal) {
+                HStack {
+                    VStack{
+                        VStack {
+                            VStack {
+                                Text("HOJE")
+                                    .frame(width: 163, height: 25)
+                                    .font(.system(size: 12, weight: .medium, design: .default))
+                                    .background(Color(UIColor(red: 0.588, green: 0.612, blue: 0.729, alpha: 1)))
+                                    .foregroundColor(Color.white)
+                                
+                                VStack {
+                                    Text("01")
+                                        .font(.system(size: 30, weight: .bold, design: .default))
+                                    
+                                    Text("Qua")
+                                }.padding(.vertical, 6)
+                                
+                            }
+                            .frame(width: 163, height: 99)
+                            .border(Color(UIColor(red: 0.588, green: 0.612, blue: 0.729, alpha: 1)), width: 3)
+                            .cornerRadius(10)
+                        }
+                        
+                        
+                        // REPEAT THIS HSTACK: |x||x||x||x||x| ... 
+                        HStack {
+                            VStack {
+                                Text("38,2")
+                                Text("Sintomas")
+                                Text("·Febre")
+                                Text("·Tosse")
+                                Text("·Dor de garganta")
+                                
+                            }
+                            .frame(width: 163, height: 248)
+                            
+                            Spacer()
+                                .frame(width: 1, height: 248)
+                                .background(Color(UIColor.lightGray))
+                                .padding(.horizontal, 5)
+                                
+                        }
+                    }
+ 
+                    
+                }
+            }
         }
     }
 }
