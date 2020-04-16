e768103e105819d52ac52f3d37f141a701e6f29c
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-04-16 18:52:53 -0300

    create basic animation of button

--- CoronaUI/CoronaUI/Views/Pacientes/PatientList/PacientesDetails/SecondCardSubview/SecondCardTopBar.swift
+++ CoronaUI/CoronaUI/Views/Pacientes/PatientList/PacientesDetails/SecondCardSubview/SecondCardTopBar.swift
@@ -35,69 +35,115 @@ enum SubTab: String, CaseIterable {
             return "Pessoas próximas"
         }
     }
+    
+    
 }
-
 
 
 struct SubTabButton: View {
     let tab: SubTab
     
     @Binding var currentTab: SubTab
-    
+    @Binding var offset: CGFloat
     var body: some View {
         
-        Button(action: {
-            print("Selected the \(self.tab.nameOfTab) one")
-            self.currentTab = self.tab
-        }) {
-            Text("\(self.tab.nameOfTab)")
-                .fontWeight(.bold)
-                .foregroundColor(Color(UIColor(red: 0.468, green: 0.459, blue: 0.875, alpha: 1)))
-                .padding()
-            
+        
+        GeometryReader { view in
+            VStack {
+                
+                Button(action: {
+                    print("Selected the \(self.tab.nameOfTab) one")
+                    self.currentTab = self.tab
+                    self.offset =   view.frame(in: .global).midX
+                    
+                }) {
+                    Text("\(self.tab.nameOfTab)")
+                        .fontWeight(.bold)
+                        .padding()
+                }
+            }
             
         }
-        
+    }
+    
+    func getOffSet() ->CGFloat {
+        return offset
     }
 }
 
+struct CustomPageControll: View {
+    @Binding var newOffSet: CGFloat
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 10).fill().frame(width: 190, height: 4)
+                .foregroundColor(Color(UIColor(red: 0.468, green: 0.459, blue: 0.875, alpha: 1)))
+                .offset(x: newOffSet )
+        }
+        .onAppear {
+            print(self.newOffSet)
+        }
+    }
 
+}
 
 struct SecondCardDetailTopBar: View {
     
+    @State private var offset: CGFloat = 0
     @State var currentTab: SubTab
+    @State var selected: Bool = false
     
-    var body: some View {
-        VStack {
-            HStack {
-                SubTabButton(tab: .dadosSintomas, currentTab: $currentTab)
-                
-                Spacer()
-                
-                SubTabButton(tab: .mensagens, currentTab: $currentTab)
-                Spacer()
-                
-                SubTabButton(tab: .acompanhamento, currentTab: $currentTab)
-                Spacer()
-                
-                SubTabButton(tab: .notas, currentTab: $currentTab)
-                Spacer()
-                
-                SubTabButton(tab: .pessoasProximas, currentTab: $currentTab)
-            }
-            .frame(width: 1156,height: 96)
-            .background(Color(UIColor.systemBackground))
-            .cornerRadius(10)
-            .shadow(color: Color.secondary, radius: 0.5, x: 0, y: 0.5)
-            .font(.system(size: 20))
-            .foregroundColor(Color(UIColor.black))
-            
-            
-            VStack{
-                getView(for: currentTab)
-            }
+    var offSetX: CGFloat {
+        switch currentTab {
+        case .dadosSintomas:
+            return offset
+        case .acompanhamento:
+            return offset
+        case .mensagens: return offset
+        case .pessoasProximas: return offset
+        case .notas:
+            return offset
         }
     }
+    var body: some View {
+        
+            VStack {
+                HStack {
+                    SubTabButton(tab: .dadosSintomas, currentTab: self.$currentTab, offset: $offset)
+                    
+                    Spacer()
+                    
+                    SubTabButton(tab: .mensagens, currentTab: self.$currentTab, offset: $offset)
+                    Spacer()
+                    
+                    SubTabButton(tab: .acompanhamento, currentTab: self.$currentTab, offset: $offset)
+                    Spacer()
+                    
+                    SubTabButton(tab: .notas, currentTab: self.$currentTab,  offset: $offset)
+                    Spacer()
+                    
+                    SubTabButton(tab: .pessoasProximas, currentTab: self.$currentTab,  offset: $offset)
+                    
+                }
+                    
+                .frame(width: 1156,height: 96)
+                .background(Color(UIColor.systemBackground))
+                .cornerRadius(10)
+                .shadow(color: Color.secondary, radius: 0.5, x: 0, y: 0.5)
+                .font(.system(size: 20))
+                .foregroundColor(Color(UIColor.black))
+                
+               
+                
+                CustomPageControll(newOffSet: $offset).animation(.default)
+                
+                VStack{
+                    self.getView(for: self.currentTab)
+                }
+            }
+        
+    }
+    
+   
     
     
     func getView(for tab: SubTab) -> AnyView {
